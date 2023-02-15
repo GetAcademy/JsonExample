b4743cea0d1b9a5a7680e045fa59334be7c64a0c
GET Academy <terje@getacademy.no>
2023-02-15 12:09:32 +0100

    Initial commit

---
+++ JsonExample/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using JsonExample;
+
+var json = File.ReadAllText("tables.json");
+var options = new JsonSerializerOptions {PropertyNameCaseInsensitive = true};
+var tables = JsonSerializer.Deserialize<Table[]>(json, options);
+foreach (var table in tables)
+{
+    Console.WriteLine(table.Name);
+}
---
+++ JsonExample/Table.cs
@@ -0,0 +1,8 @@
+﻿namespace JsonExample
+{
+    internal class Table
+    {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+    }
+}
